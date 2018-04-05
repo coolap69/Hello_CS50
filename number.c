ffefbd9fb2fdc44a9ca2528bd6383311dc057ba7
Louis Pazos <louispazos@gmail.com>
2018-04-05 22:12:20 +0000

    lesson1

---
+++ number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+// int stuff(void)
+// {
+//     printf("Hello\n");
+//     return 1;
+// }
+
+// int stuff(vchar* word)
+// {
+//     printf("%s\n");
+//     return 1;
+// }
+
+int main(void)
+{
+    printf("Hello there!\n");
+    int integer = 5 + 8;
+    //000101 + 001000 = 001101
+    printf("This is my number: %i\n", integer);
+    float flt = 5.3 + 4.83;
+    printf("This is my float: %.20f\n", flt);
+}
